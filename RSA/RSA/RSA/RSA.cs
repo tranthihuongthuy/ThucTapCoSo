0f376e36d470cdec29f7f0cc3364dd1d2827f657
thirr <tranthihuongthuy27@gmail.com>
2024-01-01 21:44:09 +0700

    final

--- RSA/RSA/RSA/RSA.cs
+++ RSA/RSA/RSA/RSA.cs
@@ -2,29 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RSA
 {
-    public class RSA
+    public class TaoKhoa
     {
-        private BigInteger p, q, N, n, e, d;
-        public RSA(BigInteger p, BigInteger q)
+        protected BigInteger p, q, N, n, e, d;
+
+        public TaoKhoa(BigInteger p, BigInteger q)
         {
             this.p = p;
             this.q = q;
             this.N = p * q;
             this.n = (p - 1) * (q - 1);
-            this.e = FindE(this.n);
-            this.d = FindD(this.e, n);
+            this.e = TimE(this.n);
+            this.d = TimD(this.e, n);
         }
-        // Hàm tìm số e nguyên tố cùng nhau với n
-        private BigInteger FindE(BigInteger n)
+
+        protected virtual BigInteger TimE(BigInteger n)
         {
             for (BigInteger i = 2; i < n; i++)
             {
-                if (BigInteger.GreatestCommonDivisor(i, n) == 1)
+                if (BigInteger.GreatestCommonDivisor(i, n) == 1 && IsPrime(i))
                 {
                     return i;
                 }
@@ -33,103 +35,20 @@ namespace RSA
             throw new Exception("Không thể tìm số e.");
         }
 
-        // Hàm tìm số d là nghịch đảo của e trong phép modulo n
-        private static BigInteger FindD(BigInteger e, BigInteger n)
+        protected virtual BigInteger TimD(BigInteger e, BigInteger n)
         {
             BigInteger d = 0;
-            for (BigInteger k = 1; ; k++)
+            for (BigInteger k = 2; ; k++)
             {
                 d = (k * n + 1) / e;
 
-                if ((d * e) % n == 1)
+                if ((d * e) % n == 1 && d != e) // Thêm điều kiện để đảm bảo d khác e
                 {
                     return d;
                 }
             }
         }
 
-        // Lấy khóa công khai (e, N)
-        public Tuple<BigInteger, BigInteger> LayKhoaCongKhai()
-        {
-            return Tuple.Create(e, N);
-        }
-
-        // Lấy khóa riêng (d, N)
-        public Tuple<BigInteger, BigInteger> LayKhoaRieng()
-        {
-            return Tuple.Create(d, N);
-        }
-        
-    }
-    class ChuongTrinh
-    {
-        static void Main()
-        {
-            Console.InputEncoding = Encoding.UTF8;
-            Console.OutputEncoding = Encoding.UTF8;
-            // Bước 1: Chọn p và q
-            Console.WriteLine("Nhập giá trị p (số nguyên tố): ");
-            BigInteger p = BigInteger.Parse(Console.ReadLine());
-
-            Console.WriteLine("Nhập giá trị q (số nguyên tố khác p): ");
-            BigInteger q = BigInteger.Parse(Console.ReadLine());
-
-            // Bước 2: Tính N
-            Console.WriteLine("Sinh khóa: ");
-            BigInteger N = p * q;
-            Console.WriteLine("N: " + N);
-            BigInteger n = (p - 1) * (q - 1);
-            Console.WriteLine("n: " + n);
-            // Bước 3: Tìm e
-            BigInteger e = FindE(n);
-            Console.WriteLine("e: " + e);
-
-            // Bước 4: Tìm d (nghịch đảo modulo)
-            BigInteger d = FindD(e, n);
-            Console.WriteLine("d: " + d);
-
-            // Bước 5: Hủy bỏ n, p và q (Đã được thực hiện trong constructor)
-            // Bước 6: Lấy khóa công khai và khóa riêng
-            var khoaCongKhai = Tuple.Create(e, N);
-            var khoaRieng = Tuple.Create(d, N);
-            Console.WriteLine("Khóa công khai: " + khoaCongKhai);
-            Console.WriteLine("Khóa riêng tư: " + khoaRieng);
-
-            BigInteger message = 3;
-            // Bước 7: Mã hóa và giải mã bảo mật
-            BigInteger encryptedMessage = MaHoa(message, e, N);
-            BigInteger decryptedMessage = GiaiMa(encryptedMessage, d, N);
-
-            Console.WriteLine("Thông điệp đã mã hóa: " + encryptedMessage);
-            Console.WriteLine("Thông điệp đã giải mã: " + decryptedMessage);
-            // Bước 7: Mã hóa và giải mã chứng thực
-            BigInteger encrypted = MaHoa(message, d, N);
-            BigInteger decrypted = GiaiMa(encrypted, e, N);
-
-            Console.WriteLine("Thông điệp đã mã hóa: " + encrypted);
-            Console.WriteLine("Thông điệp đã giải mã: " + decrypted);
-            Console.ReadKey();
-        }
-
-
-        private static BigInteger FindE(BigInteger n)
-        {
-            for (BigInteger e = 2; e < n; e++)
-            {
-                if (BigInteger.GreatestCommonDivisor(e, n) == 1)
-                {
-                    // Nếu e là số nguyên tố cùng nhau với n, trả về giá trị e
-                    if (IsPrime(e))
-                    {
-                        return e;
-                    }
-                }
-            }
-
-            throw new Exception("Không thể tìm số e.");
-        }
-
-        // Hàm kiểm tra xem một số có phải là số nguyên tố không
         private static bool IsPrime(BigInteger number)
         {
             if (number <= 1)
@@ -148,32 +67,215 @@ namespace RSA
             return true;
         }
 
-        private static BigInteger FindD(BigInteger e, BigInteger N)
+        public Tuple<BigInteger, BigInteger> LayKhoaCongKhai()
+        {
+            return Tuple.Create(e, N);
+        }
+
+        public Tuple<BigInteger, BigInteger> LayKhoaRieng()
+        {
+            return Tuple.Create(d, N);
+        }
+        public BigInteger GetN()
+        {
+            return n;
+        }
+
+        public BigInteger GetNModulus()
+        {
+            return N;
+        }
+
+        public BigInteger GetE()
+        {
+            return e;
+        }
+
+        public BigInteger GetD()
+        {
+            return d;
+        }
+    }
+
+    public class RSAEncryptor : TaoKhoa
+    {
+        public RSAEncryptor(BigInteger p, BigInteger q) : base(p, q)
+        {
+        }
+
+        protected override BigInteger TimE(BigInteger n)
+        {
+            for (BigInteger i = 2; i < n; i++)
+            {
+                if (BigInteger.GreatestCommonDivisor(i, n) == 1 && IsPrime(i))
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception("Không thể tìm số e trong RSAEncryptor.");
+        }
+
+        private static bool IsPrime(BigInteger number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            for (BigInteger i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        protected override BigInteger TimD(BigInteger e, BigInteger n)
         {
             BigInteger d = 0;
-            for (BigInteger k = 1; ; k++)
+            for (BigInteger k = 2; ; k++)
             {
-                d = (k * N + 1) / e;
+                d = (k * n + 1) / e;
 
-                if ((d * e) % N == 1)
+                if ((d * e) % n == 1 && d != e) // Thêm điều kiện để đảm bảo d khác e
                 {
                     return d;
                 }
             }
         }
-        private static BigInteger MaHoa(BigInteger M, BigInteger e, BigInteger n)
-        {
-            BigInteger encryptedMessage = BigInteger.ModPow(M, e, n);
-            return encryptedMessage;
-        }
 
-        private static BigInteger GiaiMa(BigInteger encryptedMessage, BigInteger d, BigInteger n)
+        public BigInteger MaHoaBaoMat(BigInteger M)
         {
-            BigInteger decryptedMessage = BigInteger.ModPow(encryptedMessage, d, n);
-            return decryptedMessage;
+            return BigInteger.ModPow(M, e, N);
+        }
+        public BigInteger MaHoaChungThuc(BigInteger M)
+        {
+            return BigInteger.ModPow(M, d, N);
+        }
+        public BigInteger TaoSoNgauNhien()
+        {
+            BigInteger maxM = N - 1;
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] randomBytes = new byte[maxM.ToByteArray().Length];
+                rng.GetBytes(randomBytes);
+
+                BigInteger randomM = new BigInteger(randomBytes);
+                randomM = BigInteger.Abs(randomM) % maxM + 1;
+
+                return randomM;
+            }
         }
     }
 
+    public class RSADecryptor : TaoKhoa
+    {
+        public RSADecryptor(BigInteger p, BigInteger q) : base(p, q)
+        {
+        }
+
+        protected override BigInteger TimE(BigInteger n)
+        {
+            for (BigInteger i = 2; i < n; i++)
+            {
+                if (BigInteger.GreatestCommonDivisor(i, n) == 1 && IsPrime(i))
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception("Không thể tìm số e trong RSADecryptor.");
+        }
+
+        private static bool IsPrime(BigInteger number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            for (BigInteger i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected override BigInteger TimD(BigInteger e, BigInteger n)
+        {
+            BigInteger d = 0;
+            for (BigInteger k = 2; ; k++)
+            {
+                d = (k * n + 1) / e;
+
+                if ((d * e) % n == 1 && d != e) // Thêm điều kiện để đảm bảo d khác e
+                {
+                    return d;
+                }
+            }
+        }
+        public BigInteger GiaiMaBaoMat(BigInteger encryptedMessage)
+        {
+            return BigInteger.ModPow(encryptedMessage, d, N);
+        }
+        public BigInteger GiaiMaChungThuc(BigInteger encryptedMessage)
+        {
+            return BigInteger.ModPow(encryptedMessage, e, N);
+        }
+    }
+
+    class ChuongTrinh
+    {
+        static void Main()
+        {
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Nhập giá trị p (số nguyên tố): ");
+            BigInteger p = BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhập giá trị q (số nguyên tố khác p): ");
+            BigInteger q = BigInteger.Parse(Console.ReadLine());
+
+            var taoKhoa = new TaoKhoa(p, q);
+            var khoaCongKhai = taoKhoa.LayKhoaCongKhai();
+            var khoaRieng = taoKhoa.LayKhoaRieng();
+
+            Console.WriteLine("Khóa công khai: " + khoaCongKhai);
+            Console.WriteLine("Khóa riêng tư: " + khoaRieng);
+            Console.WriteLine("n: " + taoKhoa.GetN());
+            Console.WriteLine("N: " + taoKhoa.GetNModulus());
+            Console.WriteLine("e: " + taoKhoa.GetE());
+            Console.WriteLine("d: " + taoKhoa.GetD());
+
+            int i = 1024;
+
+            var maHoa = new RSAEncryptor(p, q);
+            var giaiMa = new RSADecryptor(p, q);
+            BigInteger randomM = maHoa.TaoSoNgauNhien();
+            Console.WriteLine("Số ngẫu nhiên M thỏa mãn điều kiện: " + randomM);
+
+            BigInteger encryptedMessage = maHoa.MaHoaBaoMat(randomM);
+            Console.WriteLine("Thông điệp đã mã hóa: " + encryptedMessage);
+            BigInteger decryptedMessage = giaiMa.GiaiMaBaoMat(encryptedMessage);
+            Console.WriteLine("Thông điệp đã giải mã: " + decryptedMessage);
+
+            BigInteger encrypted = maHoa.MaHoaChungThuc(randomM);
+            Console.WriteLine("Thông điệp đã mã hóa: " + encrypted);
+            BigInteger decrypted = giaiMa.GiaiMaChungThuc(encrypted);
+            Console.WriteLine("Thông điệp đã giải mã: " + decryptedMessage);
+
+            Console.ReadKey();
+        }
+    }
 }
 
 
